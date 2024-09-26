e8859b29600b0c6619def15098e15b49d49609f4
joeljoanlopez <joeljoanlopez@gmail.com>
2024-09-26 21:21:10 +0200

    Added documentation and implemented jump

--- Assets/Scripts/Player/MovementController.cs
+++ Assets/Scripts/Player/MovementController.cs
@@ -7,26 +7,34 @@ public class MovementController : MonoBehaviour
 
     public float acceleration = 5f;
     public float maxSpeed = 10f;
+    public float jumpForce = 5f;
 
     private PlayerInput playerInput;
+    private Rigidbody rigidBody;
 
     private Vector2 moveInput;
 
     private void Start() {
         playerInput = GetComponent<PlayerInput>();
+        rigidBody = GetComponent<Rigidbody>();
     }
 
+    /// Reads the input for movement and stores it in moveInput
     public void OnMove(){
         moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
-        Debug.Log("Move Input: " + moveInput);
     }
 
+    /// Reads the input for jumping and applies an upward force
     public void OnJump(){
-
+        rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
     private void Update() {
+        //Move the character based on the input
         Vector3 targetVelocity = new Vector3(moveInput.x, 0f, moveInput.y) * maxSpeed;
-        GetComponent<Rigidbody>().velocity = Vector3.Lerp(GetComponent<Rigidbody>().velocity, targetVelocity, acceleration * Time.deltaTime);
+        rigidBody.velocity = Vector3.Lerp(GetComponent<Rigidbody>().velocity, targetVelocity, acceleration * Time.deltaTime);
+
+        //Apply gravity to the character
+        rigidBody.AddForce(Vector3.down * 9.8f, ForceMode.Acceleration);
     }
 }
