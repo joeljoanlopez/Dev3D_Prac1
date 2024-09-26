c49dbfa8dddb365a7c46c9ba6da2040b6f7ebda0
joeljoanlopez <joeljoanlopez@gmail.com>
2024-09-26 21:12:58 +0200

    Movement implemented

--- Assets/Scripts/Player/MovementController.cs
+++ Assets/Scripts/Player/MovementController.cs
@@ -1,14 +1,32 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class MovementController : MonoBehaviour {
+public class MovementController : MonoBehaviour
+{
 
     public float acceleration = 5f;
     public float maxSpeed = 10f;
 
     private PlayerInput playerInput;
 
-    public void Start(){
+    private Vector2 moveInput;
+
+    private void Start() {
         playerInput = GetComponent<PlayerInput>();
+    }
+
+    public void OnMove(){
+        moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+        Debug.Log("Move Input: " + moveInput);
+    }
+
+    public void OnJump(){
+
+    }
+
+    private void Update() {
+        Vector3 targetVelocity = new Vector3(moveInput.x, 0f, moveInput.y) * maxSpeed;
+        GetComponent<Rigidbody>().velocity = Vector3.Lerp(GetComponent<Rigidbody>().velocity, targetVelocity, acceleration * Time.deltaTime);
     }
 }
