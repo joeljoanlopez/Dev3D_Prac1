c133eb49ff21e33cdf68e1fa7b7f5a561b0ff48f
joeljoanlopez <joeljoanlopez@gmail.com>
2024-09-29 11:56:51 +0200

    Added vfx to shoot action

--- Assets/Scripts/ActionController.cs
+++ Assets/Scripts/ActionController.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class ActionController : MonoBehaviour {
+    [Header ("Controllers")]
     public GameObject player;
     public Camera camera;
+
+    [Header ("Effects")]
+    public GameObject bulletEffect;
+    public ParticleSystem shootEffect;
     
     [Header ("Gun")]
     public float damage = 10f;
     public float range = 100f;
-    public float shootRate = 15f;
+    public float shootRate = 0.5f;
     private float shootTimer = 0f;
+
 
     private PlayerInput playerInput;
 
@@ -19,15 +25,24 @@ public class ActionController : MonoBehaviour {
 
 
     private void Update() {
-        if (playerInput.actions["Shoot"].WasPressedThisFrame()) {
+        shootTimer -= Time.deltaTime;
+        bool canShoot = playerInput.actions["Shoot"].ReadValue<float>() == 1 && shootTimer <= 0f;
+        if (canShoot) {
             Shoot();
+            shootTimer = shootRate;
         }
     }
 
     private void Shoot(){
+        shootEffect.Play();
         RaycastHit hit;
         if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range)){
             Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null) 
+                target.TakeDamage(damage);
         }
+
+        GameObject bullet = Instantiate(bulletEffect, hit.point, Quaternion.identity);
     }
 }
