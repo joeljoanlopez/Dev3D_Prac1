b7664a5d2f8392d91f927896375ad6f9f7698bc9
joeljoanlopez <joeljoanlopez@gmail.com>
2024-10-11 16:03:02 +0200

    Added Attack and Chase state scripts

--- Assets/Scripts/AI/States/AttackState.cs
+++ Assets/Scripts/AI/States/AttackState.cs
@@ -1,20 +1,70 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
-[CreateAssetMenu (fileName = "AttackState", menuName = "FSM/AttackState")]
+[CreateAssetMenu(fileName = "AttackState", menuName = "FSM/AttackState")]
 public class AttackState : StateBlueprint
 {
+    [Header("Attack Parameters")]
+    public float damage = 10f;
+    public float range = 10f;
+    public float fireRate = 1f;
+    private float nextFire;
+
     public override void OnEnter(FSM fsm)
     {
-        throw new System.NotImplementedException();
+        ResetCooldown();
     }
 
     public override void OnStay(FSM fsm)
     {
-        throw new System.NotImplementedException();
+        if (IsNull(fsm.player)) return;
+        
+        UpdateCooldown();
+        
+        if (nextFire <= 0)
+        {
+            Attack(fsm.player);
+            ResetCooldown();
+        }
     }
 
     public override void OnExit(FSM fsm)
     {
-        throw new System.NotImplementedException();
+        // No specific exit logic
+    }
+
+    private void Attack(GameObject target)
+    {
+        var healthManager = target.GetComponent<HealthManager>();
+
+        if (healthManager != null)
+        {
+            healthManager.TakeDamage(damage);
+            nextFire = fireRate; // Restart cooldown
+        }
+        else
+        {
+            Debug.LogWarning("Target does not have a Health Manager");
+        }
+    }
+
+    private void UpdateCooldown()
+    {
+        nextFire -= Time.deltaTime;
+    }
+
+    private void ResetCooldown()
+    {
+        nextFire = fireRate;
+    }
+
+    private bool IsNull(GameObject target)
+    {
+        if (target == null)
+        {
+            Debug.LogWarning("FSM player is null.");
+            return true;
+        }
+        return false;
     }
 }
--- Assets/Scripts/AI/States/ChaseState.cs
+++ Assets/Scripts/AI/States/ChaseState.cs
@@ -1,9 +1,54 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ChaseState", menuName = "FSM/ChaseState", order = 0)]
 public class ChaseState : StateBlueprint
 {
-    public override void OnEnter(FSM fsm) { }
-    public override void OnStay(FSM fsm) { }
-    public override void OnExit(FSM fsm) { }
+    [Header("Distances")] 
+    public float chaseDistance = 50f;
+    public float maxChaseDistance = 100f;
+    private float currentChaseDistance;
+
+    [Header("Movement")] public float speed = 10f;
+
+    [Header("Info")] 
+    private Vector3 playerPosition;
+    private Vector3 currentPosition;
+    private Transform myTransform;
+
+    public override void OnEnter(FSM fsm)
+    {
+        myTransform = fsm.transform;
+    }
+
+    public override void OnStay(FSM fsm)
+    {
+        UpdatePositions(fsm);
+        MoveTowardsTarget();
+        if (currentChaseDistance < chaseDistance)
+        {
+            fsm.ChangeState("Attack");
+        }
+        else if (currentChaseDistance > maxChaseDistance)
+        {
+            fsm.ChangeState("Alert");
+        }
+    }
+
+    public override void OnExit(FSM fsm)
+    {
+        // Nothing
+    }
+
+    private void UpdatePositions(FSM fsm)
+    {
+        playerPosition = fsm.player.transform.position;
+        currentPosition = myTransform.position;
+        currentChaseDistance = Vector3.Distance(playerPosition, currentPosition);
+    }
+
+    private void MoveTowardsTarget()
+    {
+        myTransform.position = Vector3.MoveTowards(currentPosition, this.playerPosition, speed * Time.deltaTime);
+    }
 }
