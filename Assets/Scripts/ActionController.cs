7ec6cb8eeaf40d92699852b50490fcc717b847da
joeljoanlopez <joeljoanlopez@gmail.com>
2024-10-16 18:33:00 +0200

    Do not call target on actioncontroller (will be removed)

--- Assets/Scripts/ActionController.cs
+++ Assets/Scripts/ActionController.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class ActionController : MonoBehaviour {
-    [Header ("Controllers")]
+public class ActionController : MonoBehaviour
+{
+    [Header("Controllers")]
     public GameObject player;
     public Camera camera;
 
-    [Header ("Effects")]
+    [Header("Effects")]
     public GameObject bulletEffect;
     public ParticleSystem shootEffect;
-    
-    [Header ("Gun")]
+
+    [Header("Gun")]
     public float damage = 10f;
     public float range = 100f;
     public float shootRate = 0.5f;
@@ -19,27 +20,33 @@ public class ActionController : MonoBehaviour {
 
     private PlayerInput playerInput;
 
-    private void Start(){
+    private void Start()
+    {
         playerInput = player.GetComponent<PlayerInput>();
     }
 
 
-    private void Update(){
+    private void Update()
+    {
         shootTimer -= Time.deltaTime;
         bool canShoot = playerInput.actions["Shoot"].ReadValue<float>() == 1 && shootTimer <= 0f;
-        if (canShoot){
+        if (canShoot)
+        {
             Shoot();
             shootTimer = shootRate;
         }
     }
 
-    private void Shoot(){
+    private void Shoot()
+    {
         shootEffect.Play();
         RaycastHit hit;
-        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range)){
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range))
+        {
             Target target = hit.transform.GetComponent<Target>();
-            if(target != null) {
-                target.TakeDamage(damage, this.gameObject);
+            if (target != null)
+            {
+                //target.TakeDamage(damage, this.gameObject);
                 GameObject bullet = Instantiate(bulletEffect, hit.point, Quaternion.identity);
                 Destroy(bullet, 1f);
             }
