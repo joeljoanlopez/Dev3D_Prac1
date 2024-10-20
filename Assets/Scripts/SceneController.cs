3f7261cfc6b82c10e5fcff095562aaaf575860fc
joeljoanlopez <joeljoanlopez@gmail.com>
2024-10-20 21:12:45 +0200

    Change scenes to strings and build

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -4,25 +4,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    public SceneAsset sceneAsset;
-    private string sceneName;
-    private void OnValidate()
-    {
-        if (sceneAsset != null)
-        {
-            sceneName = sceneAsset.name;
-        }
-    }
+    public string scene;
     private void OnTriggerEnter(Collider other)
     {
-        if (!string.IsNullOrEmpty(sceneName))
-        {
-
-            SceneManager.LoadScene(sceneName);
-        }
-        else
-        {
-            Debug.LogError("Scene name is not set, maybe you need to assign a scene in the editor");
-        }
+        SceneManager.LoadScene(scene);
     }
 }
